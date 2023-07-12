789a0db2258c6659f73d1cf9b09e1451b82edb29
krasapchik <kutman_mamytov359@gmail.com>
2023-07-12 23:51:39 +0300

    first commit

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import classes from "./Menu.module.css"
+import {Link} from "react-router-dom"
 const Menu = () => {
   return (
     <div className={classes.photo}>
         <div className={classes.round}>
             <div className={classes.php}>
+            <Link to="/music">
             <img  className={classes.img} src= "/music/bakr.jpg" alt='horse'/>
-            </div>
+       </Link>      </div>
          <div className={classes.str}>
             <p>lololo</p>
          </div>
@@ -14,16 +16,18 @@ const Menu = () => {
       
         <div className={classes.round}>
             <div className={classes.php}>
+               <Link to = "/politic">
             <img  className={classes.img1} src= "/politic/putin.jpg" alt='horse'/>
-            </div>
+          </Link>  </div>
          <div className={classes.str}>
             <p>lololo</p>
          </div>
         </div>
         <div className={classes.round}>
             <div className={classes.php}>
+               <Link to = "/sport">
             <img  className={classes.img} src= "/sport/box.jpg" alt='horse'/>
-            </div>
+           </Link> </div>
          <div className={classes.str}>
             <p>lololo</p>
          </div>
